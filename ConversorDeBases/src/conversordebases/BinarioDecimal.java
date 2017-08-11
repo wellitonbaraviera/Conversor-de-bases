3a4d1cd31e41d9f942949b1ffcc9fe52b77e55a6
Welliton <welliton.silva@catolicasc.or.br>
2017-08-11 17:21:31 -0300

    Classe BinarioDecimal primeira modificação.

--- ConversorDeBases/src/conversordebases/BinarioDecimal.java
+++ ConversorDeBases/src/conversordebases/BinarioDecimal.java
@@ -5,10 +5,24 @@
  */
 package conversordebases;
 
+import java.util.Scanner;
+
 /**
  *
  * @author welliton
  */
 public class BinarioDecimal {
-    
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String entrada = leitor.next();
+        int potencia = 0;
+        int decimal = 0;
+        for (int i = entrada.length() - 1; i >= 0; i--) {
+            decimal += Math.pow(2, potencia)
+                    * Character.getNumericValue(entrada.charAt(i));
+            potencia++;
+        }
+        System.out.println(decimal);
+    }
 }
