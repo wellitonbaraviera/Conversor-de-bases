4b6cc8a571ddcba31427c3971a832d15b7db58c9
Welliton <welliton.silva@catolicasc.or.br>
2017-08-14 20:41:00 -0300

    Criando classe HexadecimalDecimal

--- ConversorDeBases/src/conversordebases/DecimalHexadecimal.java
+++ ConversorDeBases/src/conversordebases/DecimalHexadecimal.java
@@ -5,10 +5,37 @@
  */
 package conversordebases;
 
+import java.util.Scanner;
+
 /**
  *
  * @author welliton
  */
 public class DecimalHexadecimal {
+    public void decimalHexadecimal(){
     
+     Scanner num = new Scanner(System.in);
+        System.out.println("Entrar com numero decimal: ");
+        int decimal = num.nextInt();
+        
+        String hexadecimal = "";
+        
+        while(decimal != 0){
+        int hexValue = decimal % 16;
+        char hexChar;
+        
+        if(hexValue <= 9 && hexValue >= 0){
+        hexChar = (char)(hexValue + '0');
+        
+        }else{
+        
+            hexChar =(char)(hexValue -10 + 'A');
+        }
+        hexadecimal = hexChar + hexadecimal;
+        decimal = decimal / 16;
+        }
+        System.out.println("Numero hexadecimal: " + hexadecimal);
+       
+    
+    }
 }
