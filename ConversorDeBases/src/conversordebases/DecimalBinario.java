c2c32177d569e992308c99cbfac296ca93f04052
Welliton <welliton.silva@catolicasc.or.br>
2017-08-14 21:38:31 -0300

    Criando classe DecimalBinario

--- ConversorDeBases/src/conversordebases/DecimalBinario.java
+++ ConversorDeBases/src/conversordebases/DecimalBinario.java
@@ -5,6 +5,8 @@
  */
 package conversordebases;
 
+import java.util.Scanner;
+
 /**
  *
  * @author welliton
@@ -13,6 +15,17 @@ public class DecimalBinario {
    
     //public void decimalBinario()
     public static void main(String[] args) {
+         Scanner leitor = new Scanner(System.in);
+        int resto = 0;
+        System.out.println("Digite o numero decimal: ");
+        String num = leitor.nextLine();
+        int cociente = Integer.parseInt(num);
+        
+        while(cociente != 0){
+            resto = cociente % 2;
+            cociente = cociente / 2;
+            
+            System.out.print(resto);
         
     }
 }
